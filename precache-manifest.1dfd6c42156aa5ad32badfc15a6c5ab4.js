aaa427b47eba34307e59f9755a6dd29d0c6eebf8
Christine Trant <christinetrant@gmail.com>
2020-06-10 20:47:07 +0100

    Updates

---
+++ precache-manifest.1dfd6c42156aa5ad32badfc15a6c5ab4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "406dbe8eb4b3c6ac13f539132dfa6873",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "caadcc3bebf051038dbc",
+    "url": "/robofriends/static/css/main.a8592ee6.chunk.css"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/robofriends/static/js/2.88b08eb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.88b08eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caadcc3bebf051038dbc",
+    "url": "/robofriends/static/js/main.3d40b414.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  },
+  {
+    "revision": "f7f384c3550765aafa4211ff3cce79cf",
+    "url": "/robofriends/static/media/DAYPBL__.f7f384c3.ttf"
+  }
+]);
